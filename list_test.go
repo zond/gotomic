00b6ba3ca238f1787bd85201395140898de131f6
Martin Bruse <martin@oort.se>
2012-06-26 13:55:29 +0200

    ..

--- list_test.go
+++ list_test.go
@@ -10,6 +10,15 @@ func (self s) Compare(o interface{}) int {
 	return 0
 }
 
+func fiddle(nr *nodeRef, do, done chan bool) {
+	<- do
+	for i := 0; i < 1000; i++ {
+		nr.push(s(fmt.Sprint("x", i)))
+		nr.pop()
+	}
+	done <- true
+}
+
 func TestTest(t *testing.T) {
 	nr := new(nodeRef)
 	fmt.Println(nr)
@@ -29,4 +38,15 @@ func TestTest(t *testing.T) {
 	nr.push(s("4"))
 	nr.push(s("5"))
 	fmt.Println(nr)
+	do := make(chan bool)
+	done := make(chan bool)
+	go fiddle(nr, do, done)
+	go fiddle(nr, do, done)
+	go fiddle(nr, do, done)
+	close(do)
+	<-done
+	<-done
+	<-done
+	fmt.Println(nr)
 }
+
