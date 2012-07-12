2e57ba070656143647a2b42c8971c901847adf7e
Martin Bruse <martin@oort.se>
2012-07-12 13:22:11 +0200

    comments

--- log.go
+++ log.go
@@ -3,24 +3,24 @@ package gotomic
 /*
  Ripped from http://graphics.stanford.edu/~seander/bithacks.html#IntegerLogLookup
 */
-var LOG_TABLE_256 []uint32
+var log_table_256 []uint32
 
 func init() {
-	LOG_TABLE_256 = make([]uint32, 256)
+	log_table_256 = make([]uint32, 256)
 	for i := 2; i < 256; i++ {
-		LOG_TABLE_256[i] = 1 + LOG_TABLE_256[i/2]
+		log_table_256[i] = 1 + log_table_256[i/2]
 	}
 }
 func log2(v uint32) uint32 {
 	var r, tt uint32
 	if tt = v >> 24; tt != 0 {
-		r = 24 + LOG_TABLE_256[tt]
+		r = 24 + log_table_256[tt]
 	} else if tt = v >> 16; tt != 0 {
-		r = 16 + LOG_TABLE_256[tt]
+		r = 16 + log_table_256[tt]
 	} else if tt = v >> 8; tt != 0 {
-		r = 8 + LOG_TABLE_256[tt]
+		r = 8 + log_table_256[tt]
 	} else {
-		r = LOG_TABLE_256[v]
+		r = log_table_256[v]
 	}
 	return r
 }
