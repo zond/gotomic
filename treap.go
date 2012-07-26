d0387370614d961da08d016867b8b6dd3c0e2312
Martin Bruse <martin@oort.se>
2012-07-26 21:04:26 +0200

    more work in the insert

--- treap.go
+++ treap.go
@@ -4,87 +4,215 @@ package gotomic
 import (
 	"math/rand"
 	"time"
+	"sync/atomic"
+	"fmt"
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-type Treap struct {
+/*
+ Transaction controlled treap
+ */
+type treap struct {
 	root *nodeHandle
-	size int64
+}
+func (self *treap) Clone() Clonable {
+	return &treap{self.root}
 }
 
+/*
+ Non-transaction controlled "user space" type
+ */
+type Treap struct {
+	handle *Handle
+	size int64
+}
+func NewTreap() *Treap {
+	return &Treap{NewHandle(&treap{}), 0}
+}
+/*
+ Get a readable *treap from the Treap
+ */
+func (self *Treap) ropen(t *Transaction) (*treap, error) {
+	r, err := t.Read(self.handle)
+	if err != nil {
+		return nil, err
+	}
+	return r.(*treap), nil
+}
+/*
+ Get a writable *treap from the Treap
+ */
+func (self *Treap) wopen(t *Transaction) (*treap, error) {
+	r, err := t.Write(self.handle)
+	if err != nil {
+		return nil, err
+	}
+	return r.(*treap), nil
+}
+
+func (treap *Treap) Put(k Comparable, v Thing) Thing {
+	rval, err := treap.put(k, v)
+	for err != nil {
+		rval, err = treap.put(k, v)
+	}
+	return rval
+}
+func (treap *Treap) put(k Comparable, v Thing) (Thing, error) {
+	t := NewTransaction()
+	self, err := treap.ropen(t)
+	if err != nil {
+		return nil, err
+	}
+	newNode := newNodeHandle(k, v)
+	newRoot, err := self.root.insert(t, newNode)
+	if err != nil {
+		return nil, err
+	}
+	if newRoot != self.root {
+		self, err = treap.wopen(t)
+		if err != nil {
+			return nil, err
+		}
+		self.root = newRoot
+		if !t.Commit() {
+			return nil, fmt.Errorf("%v changed during put", treap)
+		}
+	}
+	atomic.AddInt64(&treap.size, 1)
+	return nil, nil
+}
+
+
 type node struct {
-	parent *nodeHandle
 	left *nodeHandle
 	right *nodeHandle
 	weight int32
 	key Comparable
 	value Thing
 }
+func (self *node) Clone() Clonable {
+	rval := *self
+	return &rval
+}
 
 type nodeHandle Handle
-func (handle *nodeHandle) ropen(t Transaction) (rval *node, err error) {
-	node, err := t.Read((*Handle)(handle))
+func (handle *nodeHandle) ropen(t *Transaction) (*node, error) {
+	n, err := t.Read((*Handle)(handle))
 	if err != nil {
 		return nil, err
 	}
-	return node.(*node), nil
+	return n.(*node), nil
 }
-func (handle *nodeHandle) wopen(t Transaction) (rval *node, err error) {
-	node, err := t.Write((*Handle)(handle))
+func (handle *nodeHandle) wopen(t *Transaction) (*node, error) {
+	r, err := t.Write((*Handle)(handle))
 	if err != nil {
 		return nil, err
 	}
-	return node.(*node), nil
+	return r.(*node), nil
 }
-func newNodeHandle(parent *nodeHandle, k Comparable, v Thing) *nodeHandle {
-	return (*nodeHandle)(NewHandle(&node{parent, nil, nil, rand.Int31(), k, v}))
+func newNodeHandle(k Comparable, v Thing) *nodeHandle {
+	return (*nodeHandle)(NewHandle(&node{nil, nil, rand.Int31(), k, v}))
 }
-func (handle *nodeHandle) rotate() {
+func (handle *nodeHandle) rotateLeft(t *Transaction) (result *nodeHandle, err error) {
+	self, err := handle.wopen(t)
+	if err != nil {
+		return
+	}
+	result = self.left
+	resultNode, err := result.wopen(t)
+	if err != nil {
+		return
+	}
+	tmp := resultNode.right
+	resultNode.right = handle
+	self.left = tmp
+	return
 }
-func (handle *nodeHandle) insert(t Transaction, k Comparable, v Thing) (err error) {
+func (handle *nodeHandle) rotateRight(t *Transaction) (result *nodeHandle, err error) {
+	self, err := handle.wopen(t)
+	if err != nil {
+		return
+	}
+	result = self.right
+	resultNode, err := result.wopen(t)
+	if err != nil {
+		return
+	}
+	tmp := resultNode.left
+	resultNode.left = handle
+	self.right = tmp
+	return
+}
+func (handle *nodeHandle) insert(t *Transaction, newHandle *nodeHandle) (result *nodeHandle, err error) {
+	if handle == nil {
+		return newHandle, nil
+	}
+	result = handle
 	self, err := handle.ropen(t)
 	if err != nil {
-		return err
+		return
 	}
-	switch cmp := k.Compare(self.key); {
+	newNode, err := newHandle.ropen(t)
+	if err != nil {
+		return 
+	}
+	switch cmp := newNode.key.Compare(self.key); {
 	case cmp < 0:
-		if self.left == nil {
-			if self, err = handle.wopen(t); err != nil {
-				return err
-			} else {
-				self.left = newNodeHandle(handle, k, v)
-				self.left.rotate()
+		var newLeft *nodeHandle
+		newLeft, err = self.left.insert(t, newHandle)
+		if err != nil {
+			return
+		}
+		if newLeft != self.left {
+			self, err = handle.wopen(t)
+			if err != nil {
+				return
 			}
-		} else {
-			if err = self.left.insert(t, k, v); err != nil {
-				return err
+			self.left = newLeft
+			var leftNode *node
+			leftNode, err = self.left.ropen(t)
+			if err != nil {
+				return
+			}
+			if leftNode.weight < self.weight {
+				result, err = handle.rotateLeft(t)
+				if err != nil {
+					return
+				}
 			}
 		}
 	case cmp > 0:
-		if self.right == nil {
-			if self, err = handle.wopen(t); err != nil {
-				return err
-			} else {
-				self.right = newNodeHandle(handle, k, v)
-				self.right.rotate()
+		var newRight *nodeHandle
+		newRight, err = self.right.insert(t, newHandle)
+		if err != nil {
+			return
+		}
+		if newRight != self.right {
+			self, err = handle.wopen(t)
+			if err != nil {
+				return
 			}
-		} else {
-			if err = self.right.insert(t, k, v); err != nil {
-				return err
+			self.right = newRight
+			var rightNode *node
+			rightNode, err = self.right.ropen(t)
+			if rightNode.weight < self.weight {
+				result, err = handle.rotateRight(t)
+				if err != nil {
+					return
+				}
 			}
 		}
-		
 	default:
 		if self, err = handle.wopen(t); err != nil {
-			return err
+			return nil, err
 		} else {
-			self.value = v
+			self.value = newNode.value
 		}
 	}	
-	return
+	return handle, nil
 }
 
 	
