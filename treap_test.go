fb838626416f632117718c4f2a0f05c06f85ee91
Martin Bruse <martin@oort.se>
2012-07-27 11:12:29 +0200

    Delete seems to work

--- treap_test.go
+++ treap_test.go
@@ -12,5 +12,14 @@ func TestFoo(t *testing.T) {
 		treap.Put(c(i), 100)
 	}
 	fmt.Println(treap.Describe())
+}
 
+func TestBar(t *testing.T) {
+	treap := NewTreap()
+	for i := 0; i < 10; i++ {
+		treap.Put(c(i), 100)
+	}
+	fmt.Println(treap.Describe())
+	treap.Delete(c(4))
+	fmt.Println(treap.Describe())
 }
