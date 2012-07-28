e7c9b208ccbd4420a3ba4a785c446ea102bdb714
Martin Bruse <martin@oort.se>
2012-07-28 12:01:28 +0200

    more testing

--- treap_test.go
+++ treap_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestPutGetDelete(t *testing.T) {
+func TestTreapPutGetDelete(t *testing.T) {
 	treap := NewTreap()
 	_, ok := treap.Get(c(3))
 	if ok {
@@ -36,5 +36,46 @@ func TestPutGetDelete(t *testing.T) {
 	}
 	if ok {
 		t.Error("should not contain 3")
+	}
+}
+
+func TestTreapMin(t *testing.T) {
+	treap := NewTreap()
+	k, v, ok := treap.Min()
+	if ok {
+		t.Error("should not have min value")
+	}
+	treap.Put(c(3), "3")
+	k, v, ok = treap.Min()
+	if !ok {
+		t.Error("should have min value")
+	}
+	if k != c(3) {
+		t.Error("min should be 3")
+	}
+	if v != "3" {
+		t.Error("min should be 3")
+	}
+	treap.Put(c(2), "2")
+	k, v, ok = treap.Min()
+	if !ok {
+		t.Error("should have min value")
+	}
+	if k != c(2) {
+		t.Error("min should be 2")
+	}
+	if v != "2" {
+		t.Errorf("min should be 2, not %#v", v)
+	}
+	treap.Put(c(4), "4")
+	k, v, ok = treap.Min()
+	if !ok {
+		t.Error("should have min value")
+	}
+	if k != c(2) {
+		t.Error("min should be 2")
+	}
+	if v != "2" {
+		t.Error("min should be 2")
 	}
 }
